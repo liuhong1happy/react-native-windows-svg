cc1389b84e9d8078ed98ef2cfb64a9b50b908650
Akshat Sharda <akshatsharda@Akshats-MacBook-Pro.local>
2017-06-06 18:39:33 +0530

    Svg export change and oneOfType warning removed

--- react-native-windows-svg.js
+++ react-native-windows-svg.js
@@ -4,8 +4,8 @@ import { requireNativeComponent, View } from 'react-native';
 var svgFace = {
     name: 'SVGView',
     propTypes: {
-        width: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        height: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        width: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        height: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
         ...View.propTypes // include the default view properties
     },
     };
@@ -14,35 +14,35 @@ var shapeFace = {
     name: 'ShapeView',
     propTypes: {
         stroke: PropTypes.string,
-        strokeWidth: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        strokeOpacity: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        strokeWidth: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        strokeOpacity: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
         //strokeLinecap: PropTypes.oneOf(['butt', 'square' , 'round']),
         //strokeLinejoin: PropTypes.oneOf(['miter', 'bevel', 'round']),
         strokeLinecap: PropTypes.string,
         strokeLinejoin: PropTypes.string,
         strokeDasharray: PropTypes.array,
-        strokeMiterlimit: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        strokeDashoffset: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        strokeMiterlimit: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        strokeDashoffset: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
         fill: PropTypes.string,
-        fillOpacity: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        fillOpacity: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
         origin: PropTypes.string,
-        originX: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        originY: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        scale: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        rotate: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        opacity: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        x: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        y: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        originX: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        originY: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        scale: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        rotate: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        opacity: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        x: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        y: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
     },
 }
 
 var rectFace = {
     name: 'RectView',
     propTypes: {
-        rx: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        ry: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        width: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        height: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        rx: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        ry: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        width: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        height: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
         ...shapeFace.propTypes,
         ...View.propTypes // include the default view properties
     },
@@ -52,10 +52,10 @@ var rectFace = {
 var lineFace = {
     name: 'LineView',
     propTypes: {
-        x1: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        y1: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        x2: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        y2: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        x1: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        y1: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        x2: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        y2: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
         ...shapeFace.propTypes,
         ...View.propTypes // include the default view properties
     },
@@ -64,9 +64,9 @@ var lineFace = {
 var circleFace = {
     name: 'CircleView',
     propTypes: {
-        cx: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        cy: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        r: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        cx: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        cy: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        r: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
         ...shapeFace.propTypes,
         ...View.propTypes // include the default view properties
     },
@@ -75,10 +75,10 @@ var circleFace = {
 var ellipseFace = {
     name: 'EllipseView',
     propTypes: {
-        cx: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        cy: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        rx: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
-        ry: PropTypes.oneOfType(PropTypes.number,PropTypes.string),
+        cx: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        cy: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        rx: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
+        ry: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
         ...shapeFace.propTypes,
         ...View.propTypes // include the default view properties
     },
@@ -121,12 +121,26 @@ var groupFace = {
     }
 }
 
-export const Svg = requireNativeComponent('RCTSVGView', rectFace);
-export const Rect = requireNativeComponent('RCTRectView', rectFace);
-export const Line = requireNativeComponent('RCTLineView', lineFace);
-export const Circle = requireNativeComponent('RCTCircleView', circleFace);
-export const Ellipse = requireNativeComponent('RCTEllipseView', ellipseFace);
-export const Polyline = requireNativeComponent('RCTPolylineView', polylineFace);
-export const Polygon = requireNativeComponent('RCTPolygonView', polygonFace);
-export const Path = requireNativeComponent('RCTPathView', pathFace);
-export const G = requireNativeComponent('RCTGroupView', groupFace);
+const Svg = requireNativeComponent('RCTSVGView', rectFace);
+const Rect = requireNativeComponent('RCTRectView', rectFace);
+const Line = requireNativeComponent('RCTLineView', lineFace);
+const Circle = requireNativeComponent('RCTCircleView', circleFace);
+const Ellipse = requireNativeComponent('RCTEllipseView', ellipseFace);
+const Polyline = requireNativeComponent('RCTPolylineView', polylineFace);
+const Polygon = requireNativeComponent('RCTPolygonView', polygonFace);
+const Path = requireNativeComponent('RCTPathView', pathFace);
+const G = requireNativeComponent('RCTGroupView', groupFace);
+
+export { 
+    Svg,
+    Rect,
+    Line,
+    Circle,
+    Ellipse,
+    Polyline,
+    Polygon,
+    Path,
+    G  
+}
+
+export default Svg
